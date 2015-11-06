9296ee86c09ff751afdd4875bfc4149bd0baa6e5
jamesfglasgow <Jamesfglasgow@gmail.com>
2015-11-06 15:40:35 +0000

    lab7 fin

--- assets/shaders/simplePostPorcessFS.glsl
+++ assets/shaders/simplePostPorcessFS.glsl
@@ -1,13 +1,22 @@
 #version 150
 
-out vec4 FragColor;
+
+//out vec4 FragColor;
 
 in vec2 textureCoords;
-
+uniform float time;
+uniform vec2 resolution;
 uniform	sampler2D texture0;
 
-void main()
+void main(void)
 {
-  FragColor = texture(texture0,	textureCoords);
+  vec2 cPos = -1.0 + 2.0 * gl_FragCoord.xy / resolution.xy;
+  float cLength = length(cPos);
+  
+  vec2 uv = gl_FragCoord.xy/resolution.xy+(cPos/cLength)*cos(cLength*12.0-time*4.0)*0.03;
+  vec3 col = texture2D(texture0,uv).xyz;
+  
+  gl_FragColor = vec4(col,1.0);
+  //FragColor = texture(texture0,	textureCoords).bbba;
 }
 
